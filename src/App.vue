d314d28f5c069280c946bd0c0c942dcd19331ee7
briantam23 <briantam23@gmail.com>
2019-10-06 07:04:11 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <li class="nav-item">
+      <router-link class="nav-link" to="/blog">Blog</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/services">Services</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/contact">contact</router-link>
+    </li>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/contact.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="contact">
+  <h1>{{title}}</h1>
+ </div>
+</template>
+<script>
+export default {
+  name: 'contact',
+  data () {
+    return {
+      title: 'contact'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
